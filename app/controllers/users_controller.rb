f9b3b49256ae7285293e160cdb7818825ab965d0
addod19 <addodaniellarbi@gmail.com>
2019-12-20 08:03:52 +0000

    created methods for users controller and update routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+    def index
+    end
+    def show
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'users#index'
   devise_for :users
+  # devise_for :users, controllers: {
+  #   sessions: 'users/sessions'
+  # }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: 'home#index'
+  
+  # resources :users, only: [:index, :show]
 end
